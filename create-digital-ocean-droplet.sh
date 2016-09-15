b84548336a210a7aaa8aa3d3ecd045d0f8c969e6
kshitizlondon <krn.world@gmail.com>
2016-09-15 11:40:36 +0100

    added json files and jq practice file to test few things

--- create-digital-ocean-droplet.sh
+++ create-digital-ocean-droplet.sh
@@ -30,7 +30,7 @@ echo "ID of Region $REGION_NAME is $REGION_ID"
 SIZE_ID=`curl -s "$BASE_URL/sizes" -H "Authorization: Bearer $DIGOCEAN_KEY" | jq "if .sizes[0].slug == \"512mb\" and .sizes[0].available == true then 1 else empty end"`
 echo "ID of Size $SIZE_NAME is $SIZE_ID"
 
-IMAGE_ID=`curl -s "$BASE_URL/images" -H "Authorization: Bearer $DIGOCEAN_KEY" | jq ".images | map(select(.name==\"$IMAGE_NAME\"))"`
+IMAGE_ID=`curl -s "$BASE_URL/images" -H "Authorization: Bearer $DIGOCEAN_KEY" | jq "."`
 echo "ID of Image $IMAGE_NAME is $IMAGE_ID"
 
 SSH_KEY_ID=`curl -s "$BASE_URL/account/keys" -H "Authorization: Bearer $DIGOCEAN_KEY" | jq '.ssh_keys[0].id'`
@@ -42,7 +42,7 @@ DROPLET_NAME="droplet-$TIMESTAMP"
 echo "Creating new Droplet $DROPLET_NAME with these specifications..."
 
 #todo curl -s "$BASE_URL/account/keys" -H "Authorization: Bearer $DIGOCEAN_KEY" -d
-# '{"name":"$DROPLET_NAME","region":"$REGION_ID","size":"512mb","image":"ubuntu-14-04-x64","ssh_keys":null,"backups":false,"ipv6":true,"user_data":null,"private_networking":null,"volumes": null}' "https://api.digitalocean.com/v2/droplets"
+# '{"name":"$DROPLET_NAME","region":"$REGION_ID","size":"$SIZE_NAME","image":"ubuntu-14-04-x64","ssh_keys":null,"backups":false,"ipv6":true,"user_data":null,"private_networking":null,"volumes": null}' "https://api.digitalocean.com/v2/droplets"
 
 RESULT=`curl -s "$BASE_URL/droplets/new?$AUTH&name=$DROPLET_NAME&size_id=$SIZE_ID&image_id=$IMAGE_ID&region_id=$REGION_ID&ssh_key_ids=$SSH_KEY_ID"`
 STATUS=`echo $RESULT | jq -r '.status'`
