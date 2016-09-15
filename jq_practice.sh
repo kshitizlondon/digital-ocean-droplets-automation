2a2cd365a407bebed09146dac77ecdb56742c040
kshitizlondon <krn.world@gmail.com>
2016-09-15 13:59:37 +0100

    added practice functions to read from json and get all ids in the variable

--- jq_practice.sh
+++ jq_practice.sh
@@ -2,3 +2,17 @@
 
 set -e
 
+# get response from the github api and save to the variable
+RESPONSE_FROM_GITHUB=$(curl 'https://api.github.com/repos/stedolan/jq/commits?per_page=5')
+
+# print as json all the sha key from the json
+echo ${RESPONSE_FROM_GITHUB} | jq .[].sha
+
+# read json from the file and get the key: "images", check images.json
+JSON_FROM_FILE=$(cat images.json)
+
+# saving the ids of images in a variable
+IMAGES_ID="$(echo ${JSON_FROM_FILE} | jq .images[].id)"
+
+# print all the image ids
+echo ${IMAGES_ID}
