c1de93acbf0732ea7e01e6cf785c85ffd6a6ceac
kshitizlondon <krn.world@gmail.com>
2016-11-16 18:33:02 +0000

    array work arround

---
+++ array_workaround.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#declaring array by keys
+area[11]=23
+area[13]=37
+area[51]=UFOs
+echo "value of area[11] is" ${area[11]}
+
+#declaring entire array
+area1=(1 2 3)
+echo "value of area1[0] is" ${area1[0]}
