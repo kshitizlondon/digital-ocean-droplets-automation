baad4da2dd669896f6305ef4658257683f93a191
kshitiz <mahajan.london@yahoo.com>
2016-11-16 22:33:04 -0800

    array length and element length in an array

--- array_workaround.sh
+++ array_workaround.sh
@@ -4,8 +4,16 @@
 area[11]=23
 area[13]=37
 area[51]=UFOs
+
+# displays array's value for this index. If no index is supplied, array element 0 is assumed
 echo "value of area[11] is" ${area[11]}
 echo "value of area[51] is" ${area[51]}
+
+# to find out the length of any element in the array
+echo "Element length is" ${#area[11]}
+
+# to find out how many values there are in the array
+echo "Array length is" ${#area[@]}
 
 #declaring entire array
 area1=(1 2 3)
